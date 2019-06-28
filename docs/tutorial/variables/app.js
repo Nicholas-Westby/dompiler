b1b909855b65e0280f0c3c82d5fcc07918b127a3
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2019-06-28 07:37:25 -0700

    Added tutorial for variables.

---
+++ docs/tutorial/variables/app.js
@@ -0,0 +1,23 @@
+// Import Dompiler.
+import { Dompiler } from "../../library/dompiler.js";
+
+// Initialize Dompiler.
+let {
+    compile
+} = new Dompiler().withBinding();
+
+// Variables.
+let message = "Hello World.",
+    date = new Date().toLocaleDateString();
+
+// Compile markup string into a document fragment.
+let markup = `
+        <div>
+            ${message} The date is ${date}.
+        </div>
+    `,
+    compiled = compile(markup);
+
+// Append compiled markup to container.
+let container = document.querySelector(".example-container");
+container.appendChild(compiled);
