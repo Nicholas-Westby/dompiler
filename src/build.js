76fe2ff795c6f6875c72dcee40efd6b54a55c807
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2019-06-30 14:13:10 -0700

    Added a build step to minify the browser version of the Dompiler JavaScript files.

--- src/build.js
+++ src/build.js
@@ -1,5 +1,6 @@
 // Dependencies.
-let fs = require("fs");
+let fs = require("fs"),
+    Terser = require("terser");
 
 // Variables.
 let noop = function () {};
@@ -12,6 +13,10 @@ transformExports("library/events.js", "dist/events.js");
 fs.copyFile("library/dompiler.js", "../docs/library/dompiler.js", noop);
 fs.copyFile("library/events.js", "../docs/library/events.js", noop);
 
+// Minify the files.
+minifyFile("library/dompiler.js", "dist/browser/dompiler.min.js");
+minifyFile("library/events.js", "dist/browser/events.min.js");
+
 /**
  * Converts the specified file into one that uses the type of exports
  * that Node understands.
@@ -22,5 +27,17 @@ function transformExports(inFilename, outFilename) {
     fs.readFile(inFilename, "utf8", function (err, data) {
         data = data.replace("export default", "module.exports =");
         fs.writeFile(outFilename, data, noop);
+    });
+}
+
+/**
+ * Minifies the specified JavaScript file.
+ * @param inFilename The path to the JavaScript file to minify.
+ * @param outFilename The path to the file to save the minified code to.
+ */
+function minifyFile(inFilename, outFilename) {
+    fs.readFile(inFilename, "utf8", function (err, data) {
+        let result = Terser.minify(data);
+        fs.writeFile(outFilename, result.code, noop);
     });
 }
